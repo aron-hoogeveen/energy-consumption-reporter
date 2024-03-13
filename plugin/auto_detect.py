1209dac25202e04a40bb6918339e4503072a8fc1
Rodin Haker <rodinhaker@hotmail.com>
2024-03-13 23:20:26 +0100

    XGBoost model integration complete(ish)

---
+++ plugin/auto_detect.py
@@ -0,0 +1,131 @@
+import subprocess
+import re
+import logging
+import math
+
+
+def get_cpu_info(logger: logging.Logger, verbose: bool = False):
+    if not verbose:
+        logger.disabled = True
+
+    data = {
+        'cpu-freq': None,
+        'cpu-threads': None,
+        'cpu-cores': None,
+        'tdp': None,
+        'ram': None,
+        'cpu-make': None,
+        'cpu-chips': None,
+        'architecture': None
+    }
+
+    try:
+        file_path = '/sys/class/powercap/intel-rapl/intel-rapl:0/name'
+        with open(file_path, 'r', encoding='UTF-8') as file:
+            domain_name = file.read().strip()
+            if domain_name != 'package-0':
+                raise RuntimeError(
+                    f"Domain /sys/class/powercap/intel-rapl/intel-rapl:0/name was not package-0, but {domain_name}")
+
+        file_path = '/sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_name'
+        with open(file_path, 'r', encoding='UTF-8') as file:
+            constraint_name = file.read().strip()
+            if constraint_name != 'long_term':
+                raise RuntimeError(
+                    f"Constraint /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_name was not long_term, but {constraint_name}")
+
+        file_path = '/sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_max_power_uw'
+        with open(file_path, 'r', encoding='UTF-8') as file:
+            tdp = file.read()
+            data['tdp'] = int(tdp) / 1_000_000  # type: ignore
+
+        logger.info('Found TDP: %d W', data['tdp'])
+    except Exception as err:
+        logger.info('Exception: %s', err)
+        logger.info(
+            'Could not read RAPL powercapping info from /sys/class/powercap/intel-rapl')
+
+    try:
+        cpuinfo = subprocess.check_output('lscpu', encoding='UTF-8')
+        match = re.search(r'On-line CPU\(s\) list:\s*(0-)?(\d+)', cpuinfo)
+        if match:
+            data['cpu-threads'] = int(match.group(2))+1  # type: ignore
+            logger.info('Found Threads: %d', data['cpu-threads'])
+        else:
+            logger.info('Could not find Threads. Using default None')
+
+        match = re.search(r'Socket\(s\):\s*(\d+)', cpuinfo)
+        if match:
+            data['cpu-chips'] = int(match.group(1))  # type: ignore
+            logger.info(
+                'Found Sockets: %d (will take precedence if not 0)', data['cpu-chips'])
+        else:
+            logger.info('Could not find Chips/Sockets via lscpu')
+
+        if data['cpu-chips']:
+            match = re.search(r'Core\(s\) per socket:\s*(\d+)', cpuinfo)
+            if match:
+                cores_per_socket = int(match.group(1))
+                data['cores'] = cores_per_socket * data['cpu-chips'] # type: ignore
+                logger.info('Found cores: %d ', data['cores'])
+            else:
+                logger.info('Could not find Cores. Using default None')
+
+        match = re.search(r'Model name:.*@\s*([\d.]+)\s*GHz', cpuinfo)
+        if match:
+            data['cpu-freq'] = int(float(match.group(1))*1000)  # type: ignore
+            logger.info('Found Frequency: %s', data['cpu-freq'])
+        else:
+            logger.info('Could not find Frequency. Using default None')
+
+        match = re.search(r'Model name:.*Intel\(R\)', cpuinfo)
+        if match:
+            data['make'] = 'intel'  # type: ignore
+            logger.info('Found Make: %s', data['make'])
+
+        match = re.search(r'Model name:.*AMD ', cpuinfo)
+        if match:
+            data['make'] = 'amd'  # type: ignore
+            logger.info('Found Make: %s', data['make'])
+
+        match = re.search(r'Architecture:\s*(\w+)', cpuinfo)
+        if match:
+            data['architecture'] = match.group(1)  # type: ignore
+            logger.info('Found Architecture: %s', data['architecture'])
+
+    except Exception as err:
+        logger.info('Exception: %s', err)
+        logger.info('Could not check for CPU info.')
+
+    try:
+        cpuinfo_proc = subprocess.check_output(
+            ['cat', '/proc/cpuinfo'], encoding='UTF-8', stderr=subprocess.DEVNULL)
+        match = re.findall(r'cpu MHz\s*:\s*([\d.]+)', cpuinfo_proc)
+        if match:
+            data['cpu-freq'] = round(max(map(float, match)))  # type: ignore
+            logger.info('Found assumend Frequency: %d', data['cpu-freq'])
+        else:
+            logger.info('Could not find Frequency. Using default None')
+    except Exception as err:
+        logger.info('Exception: %s', err)
+        logger.info(
+            '/proc/cpuinfo not accesible on system. Could not check for Base Frequency info. Setting value to None.')
+
+    try:
+        meminfo = subprocess.check_output(
+            ['cat', '/proc/meminfo'], encoding='UTF-8', stderr=subprocess.DEVNULL)
+        match = re.search(r'MemTotal:\s*(\d+) kB', meminfo)
+        if match:
+            data['ram'] = math.ceil(  # type: ignore
+                int(match.group(1)) / 1024 / 1024)
+            logger.info('Found Memory: %d GB', data['ram'])
+        else:
+            logger.info('Could not find Memory. Using default None')
+    except Exception as err:
+        logger.info('Exception: %s', err)
+        logger.info(
+            '/proc/meminfo not accesible on system. Could not check for Memory info. Defaulting to None.')
+
+    logger.disabled = False
+
+    return data
--- plugin/main.py
+++ plugin/main.py
@@ -1,4 +1,151 @@
+import os
+import pickle
+import threading
+import numpy as np
+import pandas as pd
+from xgboost import XGBRegressor
+from auto_detect import get_cpu_info
+import logging
+from time import sleep
+import psutil
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+
+running = True
+
+
+def train_model(cpu_chips, Z):
+
+    df = pd.read_csv(
+        os.getcwd() + '/plugin/spec_power_model/data/spec_data_cleaned.csv')
+
+    X = df.copy()
+    X = pd.get_dummies(X, columns=['CPUMake', 'Architecture'])
+
+    if cpu_chips:
+        logger.info(
+            'Training data will be restricted to the following amount of chips: %d', cpu_chips)
+
+        # Fit a model for every amount of CPUChips
+        X = X[X.CPUChips == cpu_chips]
+
+    if X.empty:
+        raise RuntimeError(
+            f"The training data does not contain any servers with a chips amount ({cpu_chips}). Please select a different amount.")
+
+    y = X.power
+
+    X = X[Z.columns]  # only select the supplied columns from the command line
+
+    logger.info(
+        'Model will be trained on the following columns and restrictions: \n%s', Z)
+
+#    params = {
+#      'max_depth': 10,
+#      'learning_rate': 0.3037182109676833,
+#      'n_estimators': 792,
+#      'min_child_weight': 1,
+#      'random_state': 762
+#    }
+    params = {}
+
+    model = XGBRegressor(**params)
+    model.fit(X, y)
+
+    return model
+
+
+def interpolate_helper(predictions, lower, upper, step=501):
+
+    diff = int(upper-lower)
+    diff_value = predictions[upper] - predictions[lower]
+
+    for i in np.linspace(0, diff, step):
+        predictions[round(lower+i, 2)] = predictions[lower] + \
+            ((diff_value/diff)*i)
+
+    return predictions
+
+
+def interpolate_predictions(predictions):
+    predictions = interpolate_helper(predictions, 0.0, 5.0, 501)
+    predictions = interpolate_helper(predictions, 5.0, 15.0, 1001)
+    predictions = interpolate_helper(predictions, 15.0, 25.0, 1001)
+    predictions = interpolate_helper(predictions, 25.0, 35.0, 1001)
+    predictions = interpolate_helper(predictions, 35.0, 45.0, 1001)
+    predictions = interpolate_helper(predictions, 45.0, 55.0, 1001)
+    predictions = interpolate_helper(predictions, 55.0, 65.0, 1001)
+    predictions = interpolate_helper(predictions, 65.0, 75.0, 1001)
+    predictions = interpolate_helper(predictions, 75.0, 85.0, 1001)
+    predictions = interpolate_helper(predictions, 85.0, 95.0, 1001)
+    # Question: between 95 and 100 is no difference. How do we extrapolate?
+    predictions = interpolate_helper(predictions, 95.0, 100.0, 501)
+
+    return predictions
+
+
+def start_measurement(pid: int):
+    global running
+
+    cpu_info = get_cpu_info(logger)
+
+    Z = pd.DataFrame.from_dict({
+        'HW_CPUFreq': [cpu_info['cpu-freq']],
+        'CPUThreads': [cpu_info['cpu-threads']],
+        'CPUCores': [cpu_info['cpu-cores']],
+        'TDP': [cpu_info['tdp']],
+        'HW_MemAmountGB': [cpu_info['ram']],
+        # 'Architecture': [cpu_info['architecture']],
+        'Architecture': ['epyc-gen3'],  # Ryzen not supported
+        'CPUMake': [cpu_info['cpu-make']],
+        'utilization': [0.0]
+    })
+
+    Z = pd.get_dummies(Z, columns=['CPUMake', 'Architecture'])
+    Z = Z.dropna(axis=1)
+
+    model: XGBRegressor
+    if not os.path.exists(os.getcwd() + '/model.pkl'):
+        logger.info('Training model')
+        model = train_model(cpu_info['cpu-chips'], Z)
+        pickle.dump(model, open('model.pkl', "wb"))
+    else:
+        logger.info('Loading model')
+        model = pickle.load(open('model.pkl', 'rb'))
+
+    process = psutil.Process(pid)
+    predictions = {}
+    counter = 0
+    while (running):
+        utilization = process.cpu_percent()
+        print("Utilization: ", utilization)
+        Z['utilization'] = float(utilization)
+        predictions[counter] = model.predict(Z)[0]
+        print("Prediction: ", predictions[counter])
+        counter += 1
+        sleep(1)
+
+    predictions = pd.DataFrame.from_dict(predictions, orient='index')
+    predictions.to_csv('predictions.csv', index=False)
+
+
+def stop_measurement():
+    global running
+    running = False
+    return 'Measurement stopped'
 
 
 if __name__ == '__main__':
-    pass
+    pid = os.getpid()
+
+    thread = threading.Thread(target=start_measurement, args=(pid,))
+    thread.start()
+
+    sleep(5)
+
+    stop_measurement()
+
+    thread.join()
